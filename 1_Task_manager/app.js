45f10d6d0e1e85e2717cc75710ac37b8cd6e063b
elm-Saad <128216930+elm-Saad@users.noreply.github.com>
2024-01-08 21:26:44 +0100

    update

--- 1_Task_manager/app.js
+++ 1_Task_manager/app.js
@@ -1,1 +1,19 @@
-console.log('Task Manager App')
+const express = require('express')
+const app = express()
+
+
+
+//static asset (middleware)
+app.use(express.static('./public'))
+
+
+
+app.get('/',(req,res)=>{
+    res.status(200).send('hi there saad')
+})
+
+
+
+app.listen(5000,()=>{
+    console.log('server is running on port 5000')
+})
