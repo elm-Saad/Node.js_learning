d68e91f7696df68b4ae287e13ae7ad7c31706e81
elm-Saad <128216930+elm-Saad@users.noreply.github.com>
2024-01-17 16:25:52 +0100

    update

--- 4_jobs_API/controllers/auth.js
+++ 4_jobs_API/controllers/auth.js
@@ -1,25 +1,23 @@
 const { StatusCodes } = require('http-status-codes')
 const User = require('../models/User')
 const {BadRequestError} = require('../errors')
-const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken')
 
 const register = async (req,res)=>{
 
-  const {name,email,password} = req.body
+  // const {name,email,password} = req.body
+  // if(!name|!email||!password){
+  //   throw new BadRequestError('please provide all data')
+  // }
 
-  if(!name|!email||!password){
-    throw new BadRequestError('please provide all data')
-  }
+  
+  const user = await User.create({...req.body})
+  // create the token
+  // const token = jwt.sign({userID:user._id,name:user.name},"jwtSecret",{expiresIn:'30d'})
 
-  const salt = await bcrypt.genSalt(10)
-  const hashPassword = await bcrypt.hash(password,salt)
-
-
-  const tempUser = {name,email,password:hashPassword}
-  // using bcrypt.js for hashing passwords
-  const user = await User.create({...tempUser})
-
-  res.status(StatusCodes.CREATED).json({user})
+  // from module 
+  const token = user.createJWT()
+  res.status(StatusCodes.CREATED).json({user:{name:user.name},token})
 
 }
 
--- 4_jobs_API/models/User.js
+++ 4_jobs_API/models/User.js
@@ -1,4 +1,6 @@
 const mongoose = require('mongoose')
+const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken')
 const UserSchema = new mongoose.Schema({
   name:{
     type:String,
@@ -22,4 +24,26 @@ const UserSchema = new mongoose.Schema({
   }
 })
 
+
+
+// handle what to do with the data coming from the user from the module
+
+UserSchema.pre('save',async function(next){
+
+  // using bcrypt.js for hashing passwords from the module 
+
+  const salt = await bcrypt.genSalt(10)
+  // the password in this docs => hash it => then when ever its cold like register on controllers/auth it will be already  hashed
+  this.password = await bcrypt.hash(this.password,salt)
+
+  next()
+})
+
+// create jwt
+UserSchema.methods.createJWT = function (){
+  // generate secure key on https://allKeysgenerator.com
+  const token = jwt.sign({userID:this._id,name:this.name},process.env.JWT_SECRET,{expiresIn:process.env.JWT_LIFETIME})
+
+  return token
+}
 module.exports = mongoose.model('User',UserSchema)
