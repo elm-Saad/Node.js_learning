4711f9de792d9e972ef73964b1db27a9c491692c
elm-Saad <128216930+elm-Saad@users.noreply.github.com>
2024-01-19 09:28:06 +0100

    update

--- 4_jobs_API/controllers/jobs.js
+++ 4_jobs_API/controllers/jobs.js
@@ -6,10 +6,25 @@ const {StatusCodes} = require('http-status-codes')
 
 
 const getAllJobs = async (req, res) => {
-  res.send('get all jobs')
+  // get all jobs created by this user on every req => and then sort them by the latest
+  const jobs = await Job.find({createdBy:req.user.userId}).sort('createdAt')
+
+  res.status(StatusCodes.OK).json({jobs,count:jobs.length})
 }
+
 const getJob = async (req, res) => {
-  res.send('get a single job')
+  // get id of the job and the userId
+  const {user:{userId},params:{id:JobId}} = req
+  const singleJob = await Job.findOne({
+    _id:JobId,
+    createdBy:userId
+  })
+
+  if(!singleJob){
+    throw new NotFoundError('No job with is '+ JobId)
+  }
+
+  res.status(StatusCodes.OK).json({singleJob})
 }
 
 const createJob = async (req, res) => {
@@ -32,11 +47,51 @@ const createJob = async (req, res) => {
 }
 
 const updateJob = async (req, res) => {
-  res.send('update a job')
+  // get id of the job and the userId and body of the req
+  const {
+    user:{userId},
+    params:{id:JobId},
+    body:{company,position}
+  } = req
+  
+
+  if(company === '' || position === ''){
+    throw new BadRequestError('company or position fields cannot be empty')
+  }
+
+
+  const job = await Job.findByIdAndUpdate({
+    _id:JobId,
+    createdBy:userId,
+  },req.body,{new: true, runValidators:true})
+
+  if(!job){
+    throw new NotFoundError('No job with is '+ JobId)
+  }
+
+  res.status(StatusCodes.OK).json({job})
 }
 
 const deleteJob = async (req, res) => {
-  res.send('delete job')
+  // get id of the job and the userId and body of the req
+  const {
+    user:{userId},
+    params:{id:JobId},
+  } = req
+
+
+
+
+  const job = await Job.findOneAndRemove({
+    _id:JobId,
+    createdBy:userId,
+  })
+
+  if(!job){
+    throw new NotFoundError('No job with is '+ JobId)
+  }
+
+  res.status(StatusCodes.OK).json({msg:'DONE'})
 }
 
 module.exports = {
