70dd9f0204cd12bd73f0661ac21c6bf6ab4d0849
elm-Saad <128216930+elm-Saad@users.noreply.github.com>
2024-01-22 16:51:53 +0100

    update

--- 5_jobster_fullStack/routes/auth.js
+++ 5_jobster_fullStack/routes/auth.js
@@ -4,10 +4,24 @@ const router = express.Router()
 const { register, login, UpdateUser } = require('../controllers/auth')
 const authenticateUser = require('../middleware/authentication')
 const testUser = require('../middleware/testUser')
+const rateLimiter = require('express-rate-limit')
+/**(addThis) API Limiter*/
+/**
+ * just for register and login 
+ * if someone Login or register from the same Ip many many times
+ */
+
+const ApiLimiter = rateLimiter({
+    windowMs: 15*60*1000,// 15 min
+    max:10,
+    message:{
+        msg:'Too many request from this Ip , please try again after 15 minutes'
+    }
+})
 
 
-router.post('/register', register)
-router.post('/login', login)
+router.post('/register',ApiLimiter, register)
+router.post('/login',ApiLimiter, login)
 /**(addThis) */
 
 /**
